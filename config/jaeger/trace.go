e8d4643a5be002d8e22ecfb2242686aec5715c5e
Longfei He <hlf513@gmail.com>
2020-05-06 19:29:33 +0800

    Update trace.go
    增加请求追踪方法

--- config/jaeger/trace.go
+++ config/jaeger/trace.go
@@ -16,3 +16,27 @@ func GetTraceId(ctx context.Context) string {
 
 	return ""
 }
+
+// StartTrace 开启请求追踪
+func StartTrace(operationName string) (opentracing.Span, context.Context) {
+	span, c := opentracing.StartSpanFromContext(context.Background(), operationName)
+	md := make(map[string]string)
+	_ = opentracing.GlobalTracer().Inject(span.Context(), opentracing.TextMap, opentracing.TextMapCarrier(md))
+	ctx := opentracing.ContextWithSpan(metadata.NewContext(c, md), span)
+
+	return span, ctx
+}
+
+// StopTrace 停止请求追踪
+func StopTrace(span opentracing.Span) {
+	if span != nil {
+		span.Finish()
+	}
+}
+
+// SetError 设置异常请求标签
+func SetError(span opentracing.Span) {
+	if span != nil {
+		span.SetTag("error", true)
+	}
+}
